93776422395389451afa34002f70b9cc99893220
vikas11009 <vikasyadav191919@gmail.com>
2016-09-04 09:19:52 -0700

    Add files via upload

---
+++ positive_number.py
@@ -0,0 +1,6 @@
+#
+x=input("enter a number")
+if x>=0:
+	print"the number entered is positive"
+else:
+	print"the number entered is negative"
---
+++ prime_numer.py
@@ -0,0 +1,8 @@
+x=input("enter number to check")
+for i in range(2,x):
+	if x%i==0:
+		print"it is not a prime number"
+		break
+	else:
+		print"its a prime number"	
+		break
